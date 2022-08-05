974dfc6257ecc6a4d94fb5a6acd90823f2178241
serhii <https://github.com/SerhiiYanovskiy/socar.git>
2022-08-05 21:30:35 +0300

    hu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -12,24 +13,26 @@ error_list = []
 def infoo():
     data = [
         {
-            'tranid': '3421125:3616162294',
+            'tranid': '',
             'post': 'НП',  # #список
-            'city': 'kvejveislvksldkva',
+            'city': 'kvejvei',
             'index': 'Київська область, с. Велика Олександрівка , вул. Озерна 44, склад №1',
             'phone': '+380 (67) 123-45-67',  # текст
             'name': 'Фамилия Имя',
             'Sum': '345,00',
             'info_status': '1-3',
             'istok': 'сайт',  # список
-            'vber': '+380501234567',
-            'field18':'Нейлон',  # Материал  #список
+            'vber': '+380507',
+            'field18':'Нейлон',
+
+            # Материал  #список
             'field231': 'Белый',  # Фон1  #список
-            'field201': '5*5см',  # Размер    #текст
+            'field201': '5см',  # Размер    #текст
             'field278': 'Красный',  # Цвет печати  #список
-            'field985': '12+3пар',  # Количество
+            'field985': 'kdijvi',  # Количество
             'kto': 'Влад',  # (Ответственный)  #список
-            'file': 'https:#tupwidget.com/e2b66c/IMG_20ilda11958553.webp',  # текст ссылки
-            'file_1': 'https:#tupwidget.com/e2b66c/IMG_20ilda11958553.webp',  # текст ссылки
+            'file': 'fkjjoiwajp',  # текст ссылки
+            'file_1': 'dkcn;lkdjcda11958553.webp',  # текст ссылки
             'file_2': 'https:#tupwidget.com/e2b66c/IMG_20ilda11958553.webp',  # текст ссылки
     },
     {
@@ -96,25 +99,21 @@ def start_webdriwer(NN):
         config.EMAIL)
     browser.find_element(by=By.XPATH, value="/html/body/div/div/div/form/div/div[2]/label/input").send_keys(
         config.PASSWORD)
-    browser.find_element(by=By.ID, value="send").click()
+    browser.find_element(by=By.ID, value="send").click()# авторизация( войти)
     time.sleep(2)
     browser.find_element(by=By.XPATH, value="/html/body/div[2]/div/div/div[2]/div[3]/button").click()
     browser.find_element(by=By.XPATH, value="/html/body/div[6]/div/div[2]/div[3]/a[2]").click()
-    browser.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/div[2]/table/tbody/tr/td[1]/a").click()
+    browser.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/div[2]/table/tbody/tr/td[1]/a").click() # dыбор таблицы
     pyautogui.moveTo(800, 800, _pause=False)
-    pyautogui.scroll(-500)
+    pyautogui.scroll(-4000)
     time.sleep(2)
-    pyautogui.scroll(-500)
+    pyautogui.scroll(-4000)
     time.sleep(2)
 
     time.sleep(1
                )
     for tranid in data:
         if len(tranid["tranid"]) < 1:
-
-            main_page = browser.page_source
-            soup = BeautifulSoup(main_page, "html")
-            soup = soup.find("span", class_="tcrm-tabs-item__cnt js-cnt-active-leads")
             browser.find_element(by=By.XPATH,
                                  value="/html/body/div[2]/div[4]/div[2]/div/div[1]/div[2]/table/tbody/tr/td[2]").click()
 
@@ -650,23 +649,30 @@ def start_webdriwer(NN):
                                  value="/html/body/div[2]/div[10]/div/div/form/div[2]/div/div/div[42]/div[2]/textarea").click()
             pyautogui.press(["pagedown"])
 
+
+            number = [random.choice('abc123' if i != 5 else 'ABC') for i in range(6)]
+            number = "".join(number)
+
+
+            browser.find_element(by=By.XPATH, value="/html/body/div[2]/div[10]/div/div/form/div[2]/div/div/div[44]/div[2]/textarea").send_keys(number)
             try:
                 browser.find_element(by=By.XPATH,
                                      value="/html/body/div[2]/div[10]/div/div/form/div[2]/div/div/div[46]/input").click()
             except:
                 print("")
             time.sleep(2)
-
-            soup1 = BeautifulSoup(main_page, "html")
-            soup1 = soup1.find("span", class_="tcrm-tabs-item__cnt js-cnt-active-leads").text
-
-            if soup == soup1:
+            main_page = browser.page_source
+            soup = BeautifulSoup(main_page, "html")
+            soup = soup.find("div", class_="tcrm-list-item tcrm-clear js-lead-item ui-draggable ui-draggable-handle")
+            if number in soup.text:
+                continue
+            else:
                 with open("error_tranid.txt", "a") as file:
                     file.write(f"{tranid}  error \n")
                 error_list.append(tranid)
-                print(f"{tranid} erorr")
-
+                print(f"{tranid} error")
                 e.msgbox(f"{tranid}An error has occured! :(", "Error")
+
 
         if len(tranid["tranid"]) > 1:
             main_page = browser.page_source
@@ -677,9 +683,6 @@ def start_webdriwer(NN):
             for elem in soup:
                 if tranid["tranid"] in elem.text:
                     line = elem.text[25::]
-                    print(line)
-
-
                     browser.find_element(by=By.ID, value=elem["id"]).click()
                     time.sleep(1)
                     try:
@@ -1340,9 +1343,7 @@ def start_webdriwer(NN):
                     for elem in soup1:
                         if tranid["tranid"] in elem.text:
                             line1 = elem.text[25::]
-                            print(line1)
                             if line1 == line:
-
                                 with open("error_tranid.txt", "a") as file:
                                     file.write(f"{tranid}  error \n")
                                 error_list.append(tranid)
@@ -1351,6 +1352,5 @@ def start_webdriwer(NN):
 
 
 start_webdriwer(0)
-
 time.sleep(300)
 start_webdriwer(1)
